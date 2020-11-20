b68f2ced0f9e377f8543ce8cb0297bc57ff89312
goshanoob <goshanoob@mail.ru>
2020-11-20 17:27:14 +0500

    Добавил программу вычисления свойств древовидной системы

---
+++ TreeSystem/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace TreeSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TreeSystem tree;
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            int[] a = baseNumbers.Text.Split(' ').Select(x => Int32.Parse(x)).ToArray();
+            tree = new TreeSystem(a);
+            List<int> ends = tree.GetEnds();
+            foreach(int i in ends)
+            {
+                listBox1.Items.Add(i);
+            }
+
+            List<List<int>> adjac = tree.GetAdjac();
+            foreach (List<int> i in adjac)
+            {
+                listBox2.Items.Add("Массив номеров смежных элементов у " + k++ +": " + string.Join(", ", i));
+            }
+
+
+            List<List<int>> supp = tree.GetSupp();
+        }
+
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void drawTree(object sender, EventArgs e)
+        {
+            Graphics grath = CreateGraphics();
+            float x = 840, y = 30, width = 20, height = 20;
+            Pen pen = new Pen(Color.Black);
+            //grath.DrawEllipse(pen, x, y, width, height);
+            /*for(int i = 1; i < tree.Bases[0]; ++i)
+            {
+                y += 30;
+                x -= 100;
+                for(int j = 0; j < tree.Adjac[i][0]; ++j)
+                {
+                    
+                    grath.DrawEllipse(pen, x, y, width, height);
+                }
+            }*/
+            recurs(1, 0);
+            void recurs(int i, int k)
+            {
+                
+                grath.DrawEllipse(pen, x-100*k, y+30*k, width, height);
+                for (int j = 1; j <= tree.Adjac[i][0]; ++j)
+                {
+                    x += 30;
+                    recurs(tree.Adjac[i][j], ++k);
+                }
+                
+            }
+            
+        }
+    }
+}
---
+++ TreeSystem/TreeSystem.cs
@@ -0,0 +1,117 @@
+﻿/* Разработать алгоритмы и оформить их в виде программы вычисления концевых, смежных элементов, 
+ * несущих цепочек для древовидной системы. Объект имеет свойства:
+ * bases = [] – массив базовых элементов;
+ * ends = [] – массив концевых элементов (первый элемент массива ends[0] содержит количество 
+ * найденных концевых элементов);
+ * adjac = [] – двумерный массив номеров смежных элементов системы; 
+ * adjac [i][j] – элемент с номером j, смежный i-му;
+ * adjac [i][0] – количество элементов, смежных i-му;
+ * supp = [] – двумерный массив номеров несущих элементов (заполняется),
+ * supp [i][j] – элемент с номером j, входящий в несущую цепочку i-го элемента;
+ * supp [i][0] – количество элементов, входящих в несущую цепочку i-го элемента.
+*/
+
+using System.Collections.Generic;
+
+namespace TreeSystem
+{
+    class TreeSystem
+    {
+        public int[] Bases { get; set; } // базовые элементы
+        public List<int> Ends { get; set; } // концевые элементы
+        public List<List<int>> Adjac { get; set; } // смежные элементы
+        public List<List<int>> Supp { get; set; } // элементы в несущей цепочке
+
+        public TreeSystem(int[] bases)
+        {
+            int N = bases.Length; // количество элементов
+            Bases = new int[N + 1];
+            Bases[0] = N;
+            bases.CopyTo(Bases, 1);
+            Ends = new List<int> { };
+            Adjac = new List<List<int>> { };
+            Supp = new List<List<int>> { };
+        }
+        public TreeSystem(int N)
+        {
+            Bases = new int[N + 1];
+            Bases[0] = N;
+            for (int i = 1; i <= N; ++i)
+            {
+                Bases[i] = i;
+            }
+            Ends = new List<int> { };
+            Adjac = new List<List<int>> { };
+            Supp = new List<List<int>> { };
+        }
+
+        public List<int> GetEnds()
+        {
+            // Поиск концевых элементов дерева. Номер элемента не встрачается в массиве Bases
+
+            bool isEnd;
+            Ends.Add(0);
+            for (int i = 1; i < Bases[0] + 1; ++i)
+            {
+                isEnd = true;
+                for (int j = i + 1; j < Bases[0] + 1; ++j)
+                {
+                    if (Bases[j] == i || Ends.Contains(i))
+                    {
+                        isEnd = false;
+                        break;
+                    }
+                }
+                if (isEnd)
+                    Ends.Add(i);
+
+            }
+            Ends[0] = Ends.Count - 1;
+            return Ends;
+        }
+
+        public List<List<int>> GetAdjac()
+        {
+            // Поиск смежных элементов. Элементы имеют один и тот же номер базового элемента
+            Adjac.Add(new List<int> { });
+            for (int i = 1; i < Bases[0] + 1; ++i)
+            {
+                Adjac.Add(new List<int> {0});
+                for (int j = i + 1; j < Bases[0] + 1; ++j)
+                {
+                    if(Bases[j] == i)
+                    {
+                        Adjac[i].Add(j);
+                    }
+                }
+
+                Adjac[i][0] = Adjac[i].Count - 1;
+
+            }
+            return Adjac;
+        }
+
+        public List<List<int>> GetSupp()
+        {
+            for(int i = 1; i < Bases[0] + 1; ++i)
+            {
+                List<int> list = new List<int> { };
+                recurs(i, ref list);
+                Supp.Add(list);
+            }
+
+            void recurs(int i, ref List<int> list)
+            {
+                list.Add(i);
+                if (Bases[i] != 0) recurs(Bases[i], ref list);
+                else
+                {
+                    list.Add(0);
+                    return ;
+                }
+                    
+            }
+            return Supp;
+        }
+    }
+}
